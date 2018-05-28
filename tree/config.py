187e5526b35d7d7a70ad5c39adb1ca2d2d906779
davidkristoffersen <david.alta2010@gmail.com>
2018-05-28 14:08:46 +0200

    Added better commenting for changing user config variables

--- tree/config.py
+++ tree/config.py
@@ -9,6 +9,17 @@ Contain variables for global access and easier debugging.
 __author__  = "David Kristoffersen"
 __email__   = "david.alta2010@gmail.com"
 
+"""
+User variables: Can be modified
+"""
+
+# Disables call tree print
+print_tree = 1
+# Determines if the call tree print should print unicode characters
+print_unicode = 1
+# Overwrite standard print
+print_polymorph = 1
+
 # Printing colors
 col_green = "\x1b[38;2;50;160;50m"
 col_yellow = "\x1b[38;2;255;204;51m"
@@ -19,23 +30,22 @@ col_bold = "\x1b[1m"
 col_faint = "\x1b[2m"
 col_reset = "\x1b[m"
 
-# Disables call tree print
-print_tree = 1
-# Determines if the call tree print should print unicode characters
-print_unicode = 1
-# Overwrite standard print
-print_polymorph = 1
+"""
+Module variables: Can not be modified
+"""
 
 # Used to check if entering a function or not
 type_enter = 0
 type_mid = 1
 type_exit = 2
 
+# Unicode box drawings
 enter_str = "┌"
 mid_str = "│"
 connect_str = "├"
 exit_str = "└"
 
+# Padding attributes
 padd_str = "─"
 padd_width = 2
 padd_len = 40
