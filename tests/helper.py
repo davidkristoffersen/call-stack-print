06cef05aa68a70575c1202881fb6c0db37912448
davidkristoffersen <david.alta2010@gmail.com>
2018-05-29 14:34:50 +0200

    Helper not including itself

--- tests/helper.py
+++ tests/helper.py
@@ -1,4 +1,4 @@
 def run_tests(glob={}):
     for key, val in glob.items():
-        if callable(val) and str(val)[:9] == "<function" and not key == "run":
+        if callable(val) and str(val)[:9] == "<function" and not key == "run_tests":
             val()
